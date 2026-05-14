5328393944bc1bac838ee14797cabde9d9adad17
princycodes <princysbhagat@gmail.com>
2026-05-14 17:12:42 +0530

    Implement binary search function in 03_merge_sort.py

---
+++ 03_merge_sort.py
@@ -0,0 +1,24 @@
+# Binary Search (Sorted Array required)
+
+def binary_search(arr, target):
+    low = 0
+    high = len(arr) - 1
+
+    while low <= high:
+        mid = (low + high) // 2
+
+        if arr[mid] == target:
+            return mid
+        elif arr[mid] < target:
+            low = mid + 1
+        else:
+            high = mid - 1
+
+    return -1
+
+arr = [10, 20, 30, 40, 50]
+target = 40
+
+result = binary_search(arr, target)
+
+print("Index:", result)
